521d0b228dd16f7eb7cbb42e776a89502d847e7a
newmanne <newman-n@hotmail.com>
2012-03-17 18:53:34 -0400

    more work being done on EMA, still very confused

--- TestSocketClient/EMA.cpp
+++ TestSocketClient/EMA.cpp
@@ -1,52 +1,47 @@
 /* EMA */
 
-using namespace std;
-class CRectangle {
-    int x, y;
-  public:
-    void set_values (int,int);
-    int area () {return (x*y);}
+class EMA {
+	double *data; //contains old info used to calculate ema
+	double prevEMA; //previous EMA
+	double currEMA; //current EMA
+	int numPeriods; //how long to wait before getting started
+	bool isEMAValid; //does currEMA have garbage?
+	double alpha; 
+	void calculateEMA();
+public:
+	EMA(int barSize, int numPeriods);
+	double getEMA();
+	bool isValid();
 };
 
-void CRectangle::set_values (int a, int b) {
-  x = a;
-  y = b;
-}
-class EMA {
-	double *ema = NULL;
-	double currEMA;
-	int numPeriods;
-	bool isEMAValid = FALSE;
-public:
-	double getEMA();
-}
-
-double EMA::EMA(int barSize, int numPeriods) {
-		this->arr = new(sizeof(double)*numPeriods);
+EMA::EMA(int barSize, int numPeriods) {
+		this->data = new double[numPeriods];
 		numPeriods = numPeriods;
 		barSize = barSize;
+		isEMAValid = false;
+		alpha = 2/(numPeriods+1); 
+}
+
+bool EMA::isValid() {
+	return isEMAValid;
 }
 
 
-EMA::calculateEMA() {
-		if (num
-
+void EMA::calculateEMA() {
+	double priceToday = 3; //how do I get this?
+	currEMA = priceToday * alpha + prevEMA * (1 - alpha);
+	numPeriods--;
 }
 
-	public double getEMA() {
-		return this->currEMA;
-	}
-
-
+double EMA::getEMA() {
+	return this->currEMA;
 }
-
-
-
-EMA = Price(Today)* k + EMA(y)* (1 – k),where k=2/(N+1)
 
 class MACD {
 	EMA *slow;
 	EMA *fast;
+	double signal;
+public:
+	MACD(int slowBars, int fastBars);
+};
 
-
-}
