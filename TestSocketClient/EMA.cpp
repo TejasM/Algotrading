4802c57a39f2ac5e8d9f016209b888d86b4ead03
newmanne <newman-n@hotmail.com>
2012-03-17 20:43:18 -0400

    less confused. How will this interact with data (for output purposes?). How will this get data?

--- TestSocketClient/EMA.cpp
+++ TestSocketClient/EMA.cpp
@@ -1,47 +1,61 @@
 /* EMA */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 class EMA {
-	double *data; //contains old info used to calculate ema
-	double prevEMA; //previous EMA
-	double currEMA; //current EMA
-	int numPeriods; //how long to wait before getting started
-	bool isEMAValid; //does currEMA have garbage?
-	double alpha; 
-	void calculateEMA();
+	vector <double> *data; //buffer for the past EMA values
+	bool isEMAValid; //really shouldn't trust data until numPeriods have passed
+	int numPeriods;
+	int barSize; //how long to wait before updating
+	double alpha; //ema constant
+	void calculateEMA(); //updates data with ema values
 public:
 	EMA(int barSize, int numPeriods);
-	double getEMA();
+	void getEMA(); //in a file? a copy of my vector?
 	bool isValid();
 };
 
-EMA::EMA(int barSize, int numPeriods) {
-		this->data = new double[numPeriods];
-		numPeriods = numPeriods;
-		barSize = barSize;
-		isEMAValid = false;
+//EMA constructor
+EMA::EMA(int barSize, int numPeriods) : barSize(barSize), numPeriods(numPeriods), isEMAValid(false) {
 		alpha = 2/(numPeriods+1); 
+		data->resize(100); //arbitrary default size
+		//request real time bar data?
 }
 
 bool EMA::isValid() {
 	return isEMAValid;
 }
 
-
 void EMA::calculateEMA() {
-	double priceToday = 3; //how do I get this?
-	currEMA = priceToday * alpha + prevEMA * (1 - alpha);
-	numPeriods--;
+	double priceToday = 3; //how do I get this?, 
+	data->push_back(priceToday * alpha + data->back() * (1 - alpha)); //calculate EMA, store it
 }
 
-double EMA::getEMA() {
-	return this->currEMA;
+void EMA::getEMA() {
+	return;
 }
+
+
 
 class MACD {
-	EMA *slow;
-	EMA *fast;
+	EMA slow;
+	EMA fast;
 	double signal;
+	double histogram;
 public:
 	MACD(int slowBars, int fastBars);
+	double getHistogram();
+	double getMACD();
+	double getSignal();
 };
 
+
+//MACD constructor
+MACD::MACD(int barSize, int numPeriods) : barSize(barSize), numPeriods(numPeriods), isEMAValid(false) {
+		alpha = 2/(numPeriods+1); 
+		data->resize(100); //arbitrary default size
+		//request real time bar data?
+}
