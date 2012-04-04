c02d2995286ad9d4bdc0e8fe5adf5f49024147f6
newmanne <newman-n@hotmail.com>
2012-04-03 23:41:53 -0400

    commiting some changes to crossover

--- TestSocketClient/EMACrossover.cpp
+++ TestSocketClient/EMACrossover.cpp
@@ -5,14 +5,18 @@
 #include<iostream>
 #include<cmath>
 #include<string>
-#include "EMACrossover.h"
 #include "Stock.h"
+#include "EMACrossover.h"
+
 
 using namespace std;
 
 //getInvestment amount and placeOrder assumed. I don't see any way for me to get valid checks. 
+EMACrossover::EMACrossover(Stock *_s, int _macdID, int _orderSize) : s(_s), macdID(_macdID), curState(INVALID), curMACD(0), orderSize(_orderSize), withStop(false) {
+}
 
-EMACrossover::EMACrossover(Stock *_s, int _macdID, int _orderSize) : s(_s), macdID(_macdID), curState(INVALID), curMACD(0), orderSize(_orderSize) {
+EMACrossover::EMACrossover(Stock *_s, int _macdID, int _orderSize, double _d1, double _d2)
+	: s(_s), macdID(_macdID), curState(INVALID), curMACD(0), orderSize(_orderSize), d1(_d1), d2(_d2), withStop(true) {
 }
 
 EMACrossover::~EMACrossover() {
@@ -24,20 +28,27 @@ state EMACrossover::getState() {
 }
 
 void EMACrossover::doEMACrossover() {
-	double macd = s->getMACD(macdID);
+	MACD *macd = s->getMACD(macdID);
+	double pi = atan((double) 1) * 4;
 
-	//check if values are valid, I'm not sure how the interface for this works
-	if (macd == INVALID)
+	//check if values are valid
+	if (!macd->isValid()) {
 		return;
+	}
 
 	double curPrice = s->getPrice();
-	//how the hell do I get fast and slow based on the currrent interface??? I also need them to calculate degrees!
-	if (curPrice > stopWin(fast,d1) {
-		placeOrder("buy", orderAmount);
-		terminate();
-	else if (price < stoLoss(slow, d2) {
-		placeOrder("sell", orderAmount);
-		terminate();
+	double fast = macd->getFast();
+	double slow = macd->getSlow();
+	double degrees = atan(fast/slow) * pi * 180;
+
+	if (withStop) {
+		if (curPrice > stopWin(fast, d1)) {
+			//placeOrder("buy", orderSize);
+			return;
+		} else if (curPrice < stopLoss(slow, d2)) {
+			/*placeOrder("sell", orderSize);*/
+			return;
+		}
 	}
 
 	if (s->getMACD(macdID) > 0) {
@@ -46,9 +57,7 @@ void EMACrossover::doEMACrossover() {
 		} else {
 			curState = FAST_ABOVE_SLOW;
 			//time to buy!
-			double degree = 0; //I'd need so much more information to get this...
-			placeOrder("buy", orderAmount);
-			terminate();
+			//placeOrder("buy", orderAmount);
 		}
 	} else {
 		if (curState == FAST_BELOW_SLOW) {
@@ -56,22 +65,22 @@ void EMACrossover::doEMACrossover() {
 		} else {
 			curState = FAST_BELOW_SLOW;
 			//time to sell!
-			placeOrder("sell", orderAmount);
-			terminate();
+			//placeorder("sell", orderamount);
 		}
 	}
 
 	return;
 }
 
-EMACrossover::stopWin(fast, d1) {
+double riskManagement(double degrees) {
+	double money = 2;
+	return (degrees/180 * money * 0.01);
+}
+
+double EMACrossover::stopWin(double fast, double  d1) {
 	return fast+d1;
 }
 
-EMACrossover::stopLoss(slow, d2) 
+double EMACrossover::stopLoss(double slow, double d2)  {
 	return slow-d2;
-}
-
-void EMACrossover::terminate() {
-	return;
 }
